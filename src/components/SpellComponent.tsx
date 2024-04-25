067c0800d5b706413e227dac2f4777c87e513da2
Ninhache <mr.carambar27@gmail.com>
2024-04-26 00:42:15 +0200

    fix: add logging & typesafety

--- src/components/SpellComponent.tsx
+++ src/components/SpellComponent.tsx
@@ -9,11 +9,17 @@ type Props = {
 };
 
 const SpellComponent: React.FC<Props> = ({ spell, onClick, selected }) => {
+
+  if (!spell.icon) {
+    console.warn(`Spell icon for ${spell.name} is missing`);
+  }
+
+
   return (<div className={`spell ${selected ? "selected-spell" : ""}`} onClick={() => { onClick(spell); }}>
     <img
       width="45"
       height="45"
-      src={spell.icon}
+      src={spell.icon || "NULL"}
       alt={spell.name}
       draggable="false"
     />
