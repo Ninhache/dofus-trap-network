383fdb8ab99fee26c91ba65ab125f79868e73636
Ninhache <mr.carambar27@gmail.com>
2024-04-26 00:43:05 +0200

    feat: update to use the "new" game hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,27 @@
-import * as React from "react";
-import "@assets/scss/App.scss";
 import "@assets/js/script.js";
-import MapComponent from "@components/MapComponent";
-import StatsComponent from "@components/StatsComponent";
-import HistoryComponent from "@components/HistoryComponent";
-import SpellsComponent from "@components/SpellsComponent";
-import Consts from "@json/Consts.json";
+import "@assets/scss/App.scss";
 import Game from "@classes/Game";
-import { GameProvider } from "@src/context/GameProvider";
+import HistoryComponent from "@components/HistoryComponent";
+import MapComponent from "@components/MapComponent";
+import SpellsComponent from "@components/SpellsComponent";
+import StatsComponent from "@components/StatsComponent";
+import Consts from "@json/Consts.json";
+import { useGame } from "@src/context/GameProvider";
 
 declare global { // Debug
   interface Window { Game: typeof Game; }
 }
 
-const App: React.FC<{ }> = ({ }) => {
+const App: React.FC<{}> = ({ }) => {
+
+  const game = useGame();
+
   return (
     <div className="app">
-      <GameProvider>
-        <StatsComponent />
-        <MapComponent cellNum={Consts.mapWidth} rowNum={Consts.mapHeight} />
-        <HistoryComponent />
-        <SpellsComponent />
-      </GameProvider>
+      <StatsComponent ref={game.statsComponent} />
+      <MapComponent ref={game.mapComponent} cellNum={Consts.mapWidth} rowNum={Consts.mapHeight} />
+      <HistoryComponent ref={game.historyComponent} />
+      <SpellsComponent ref={game.spellsComponent} />
     </div>
   );
 };
