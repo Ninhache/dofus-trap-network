51abd34800defbf56e754a65797529831e67cc59
Ninhache <mr.carambar27@gmail.com>
2024-04-26 12:44:44 +0200

    feat: adding strictmode for debug

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,14 @@ import App from "@components/App";
 import { createRoot } from "react-dom/client";
 import "@src/i18n";
 import { GameProvider } from "./context/GameProvider";
+import { StrictMode } from "react";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
-root.render(<GameProvider><App /></GameProvider>);
+root.render(
+    <StrictMode>
+    <GameProvider>
+        <App />
+    </GameProvider>
+    </StrictMode>
+);
